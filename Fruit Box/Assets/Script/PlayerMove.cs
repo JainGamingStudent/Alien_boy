d3a362466c7d30f73fe40939608b00d6aeb1330e
gokul1901 <62790600+gokul1901@users.noreply.github.com>
2020-04-28 12:32:48 +0530

    delete

---
+++ Fruit Box/Assets/Script/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    private float speed = 3f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move() {
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 temp = transform.position;
+        if (h > 0)  {
+            temp.x += speed * Time.deltaTime;
+            sr.flipX = false;
+            anim.SetBool("Walk", true);
+
+        }else if (h < 0){
+            temp.x -= speed * Time.deltaTime;
+            sr.flipX = true;
+            anim.SetBool("Walk", true);
+        }
+        else if (h == 0){
+            anim.SetBool("Walk", false);
+        }
+
+        transform.position = temp;
+    }
+
+    
+    
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "stone")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+
+}
---
+++ Fruit Box/Assets/Script/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Fruit;
+
+    private float min_X = -2.5f;
+    private float max_X = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+        
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+
+        GameObject k = Instantiate(Fruit);
+
+        float x = Random.Range(min_X, max_X);
+        k.transform.position = new Vector2(x, transform.position.y);
+
+        StartCoroutine(StartSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
